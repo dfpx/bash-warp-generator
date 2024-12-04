2d21282f3eeaa33801dc7cf894a83b83f2560f89
DFPX <dfpx@bk.ru>
2024-12-04 11:56:32 +0500

    Update warp_generator.sh

--- warp_generator.sh
+++ warp_generator.sh
@@ -46,15 +46,12 @@ Endpoint = 188.114.97.66:3138
 EOM
 )
 
-echo -e "\n"
-[ -t 1 ] && echo "################ НАЧАЛО КОНФИГА ################"
-echo -e "\n"
+[ -t 1 ] && echo -e "################ НАЧАЛО КОНФИГА ################\n"
 echo "${conf}"
-echo -e "\n"
-[ -t 1 ] && echo "################ КОНЕЦ КОНФИГА ################"
+[ -t 1 ] && echo -e "\n################ КОНЕЦ КОНФИГА ################"
 
 conf_base64=$(echo -n "${conf}" | base64 -w 0)
 echo -e "\n"
 echo "Скачать конфиг файлом:"
-echo "→ https://dfpx.github.io/downloader.html?filename=WARP.conf&content=${conf_base64} ←"
+echo "https://dfpx.github.io/downloader.html?filename=WARP.conf&content=${conf_base64}"
 echo -e "\n"
